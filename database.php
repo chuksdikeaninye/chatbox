f14bc8cf2fa7535094792f23380dd0c331e171ef
ChuksDikeAninye <chuksdikeaninye@gmail.com>
2021-05-09 10:30:49 +0100

    first commit

--- database.php
+++ database.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
 $servername = "localhost";
 $username = "root";
@@ -15,26 +13,3 @@ if (!$connect) {
 }
 
 
-=======
-=======
->>>>>>> a81707173982a565c02f76192b4eba3f214a852a
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "shoutit";
-
-// Creating connection
-$connect = mysqli_connect($servername,$username,$password,$database);
-
-// Checking connection
-if (!$connect) {
-    echo ("Connection failed:" . mysqli_connect_error());
-}
-
-
-<<<<<<< HEAD
->>>>>>> initial commit
-=======
->>>>>>> a81707173982a565c02f76192b4eba3f214a852a
-?>
