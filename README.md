d5cfa115b1edcc8ad85c0b653d442b8c16ed95db
ChuksDikeAninye <chuksdikeaninye@gmail.com>
2021-05-10 00:47:41 +0100

    first upload

--- README.md
+++
@@ -1,6 +0,0 @@
-# chatbox
-<<<<<<< HEAD
-A simple  single page web application where multiple people can chat
-"# chatbox" 
-=======
->>>>>>> a81707173982a565c02f76192b4eba3f214a852a
--- delete.php
+++ delete.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
 //including the database connection file
 include("database.php");
@@ -12,23 +10,3 @@ $result = mysqli_query($mysqli, "DELETE FROM shouts WHERE id=$id");
  
 //redirecting to the display page (index.php in our case)
 header("Location: index.php");
-=======
-=======
->>>>>>> a81707173982a565c02f76192b4eba3f214a852a
-<?php
-//including the database connection file
-include("database.php");
- 
-//getting id of the data from url
-$id = $_GET['id'];
- 
-//deleting the row from table
-$result = mysqli_query($mysqli, "DELETE FROM shouts WHERE id=$id");
- 
-//redirecting to the display page (index.php in our case)
-header("Location: index.php");
-<<<<<<< HEAD
->>>>>>> initial commit
-=======
->>>>>>> a81707173982a565c02f76192b4eba3f214a852a
-?>
