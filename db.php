cf913942cc2f30b0fe67bf5eea15514d7ec4d7d9
Chuks Dike Aninye <54310404+chuksdikeaninye@users.noreply.github.com>
2021-05-10 01:01:39 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "shoutit";
+
+// Creating connection
+$connect = mysqli_connect($servername,$username,$password,$database);
+
+// Checking connection
+if (!$connect) {
+    echo ("Connection failed:" . mysqli_connect_error());
+}
+
+
